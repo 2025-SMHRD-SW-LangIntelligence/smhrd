bcdcafcb5e966570e614831768f3e2ae779c7ac7
smhrd1 <smhrd1@192.168.219.48>
2025-06-11 11:28:06 +0900

    0611/수현 - 프로젝트 생성

---
+++ smhrd/src/main/java/com/smhrd/smhrd/SmhrdApplication.java
@@ -0,0 +1,13 @@
+package com.smhrd.smhrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmhrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmhrdApplication.class, args);
+	}
+
+}
---
+++ smhrd/src/main/java/com/smhrd/smhrd/controller/MemberController.java
@@ -0,0 +1,9 @@
+package com.smhrd.smhrd.controller;
+
+public class MemberController {
+	
+	public static void main(String[] args) {
+		System.out.println("팀장 이름");
+	}
+
+}
